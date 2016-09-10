db53c31cc47dfad2283c0c012452bdd0cc91de24
PixelMan93 <karamveersinghgrewal@gmail.com>
2016-09-11 02:38:50 +0530

    First Commit

---
+++ app/src/main/java/project/finalsem/Pixel8/CameraController/CameraControllerException.java
@@ -0,0 +1,8 @@
+package project.finalsem.Pixel8.CameraController;
+
+/** Exception for CameraController classes.
+ */
+public class CameraControllerException extends Exception {
+	private static final long serialVersionUID = 7904697847749213106L;
+
+};
---
+++ app/src/main/java/project/finalsem/Pixel8/MyDebug.java
@@ -0,0 +1,8 @@
+package project.finalsem.Pixel8;
+
+/** Global constant to control logging, should always be set to false in
+ *  released versions.
+ */
+public class MyDebug {
+	public static final boolean LOG = false;
+}
